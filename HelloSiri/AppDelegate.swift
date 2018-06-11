384269f4b3c94e667f2a33daf63dc56f8dbeee32
Rehan Parkar <parkarcamp@gmail.com>
2018-06-11 17:07:14 -0500

    connected app and delegate with siri

--- HelloSiri/AppDelegate.swift
+++ HelloSiri/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Intents
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        
+        guard let intent = userActivity.interaction?.intent as? INStartWorkoutIntent  else {
+            print("Appdelegate : Start workout intent - False")
+            return false
+        }
+        
+        print("AppDelegate: Start workiut inent - true")
+        print(intent)
+        return true
+        
+    }
+
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
