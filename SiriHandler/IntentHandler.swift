631a47de45e8fdfbe2323f8031355bf5fce18fa3
Rehan Parkar <parkarcamp@gmail.com>
2018-06-11 16:55:08 -0500

    added Intent extention

---
+++ SiriHandler/IntentHandler.swift
@@ -0,0 +1,36 @@
+//
+//  IntentHandler.swift
+//  SiriHandler
+//
+//  Created by Rehan Parkar on 2018-06-11.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension, INStartWorkoutIntentHandling {
+    
+    //properties
+    
+    
+    func handle(intent: INStartWorkoutIntent, completion: @escaping (INStartWorkoutIntentResponse) -> Void) {
+        
+        print("Start work out intent", intent)
+        
+        let userActivity: NSUserActivity? = nil
+        
+        guard let spokenPhrase = intent.workoutName?.spokenPhrase else {
+            completion(INStartWorkoutIntentResponse(code: .failureNoMatchingWorkout, userActivity: userActivity))
+            return
+        }
+        
+        if spokenPhrase == "walk" || spokenPhrase == "run" {
+            completion(INStartWorkoutIntentResponse(code: .handleInApp, userActivity: userActivity))
+        } else {
+            completion(INStartWorkoutIntentResponse(code: .failureNoMatchingWorkout, userActivity: userActivity))
+        }
+    }
+    
+    
+}
+
